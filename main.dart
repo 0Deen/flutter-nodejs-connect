02ede0b6e19645a110f9165b6bad46ba72b4480c
Kiprotich Kipchumba <01kiprotich@gmail.com>
2025-03-26 10:33:35 +0300

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+
+void main(){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  //This widget is he root of your application
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}
